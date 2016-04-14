bded6504a2c8cebeed4d6e959df6fdc98ed00e11
Trevor Pham <trevor.dt.pham@gmail.com>
2016-04-14 13:19:56 -0700

    put css on user profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def format_datetime(datetime)
+    datetime.strftime("%e %B %Y %r")
+  end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,6 +2,6 @@ class Topic < ActiveRecord::Base
   has_many :posts
   belongs_to :user
 
-  validates :title, presence: :true, length: {maximum: 30}
+  validates :title, presence: :true, length: {maximum: 55}
   validates :image_url, presence: :true
 end
